e1664bf21e52f39c8cd7bc352b77f86cd9afcaf0
Simon <simwir@hotmail.com>
2015-02-13 18:57:26 +0100

    Merged wall

--- app/src/main/java/dk/simwir/pingpong/Wall.java
+++ app/src/main/java/dk/simwir/pingpong/Wall.java
@@ -40,6 +40,7 @@ public class Wall extends ActionBarActivity implements View.OnTouchListener, Wal
         surfaceView.setOnTouchListener(this);
         setContentView(surfaceView);
 
+        //Gets the size of the screen
         metrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
 
@@ -49,14 +50,18 @@ public class Wall extends ActionBarActivity implements View.OnTouchListener, Wal
     private void createBall(){
         Random r = new Random();
         resetBall();
+        //Sets the speed of the ball to 1/1400 of the height of the display
         ballSpeed = metrics.heightPixels / 1400;
+        //Randomizes if the ball starts moving right left
         ballMoveRight = r.nextInt(2)==1;
     }
 
     private void resetBall(){
+        //Resets the ball to the center
         bx = metrics.widthPixels / 2;
         by = metrics.heightPixels / 2;
         br = metrics.heightPixels / 50;
+        //makes the ball move up
         ballMoveDown = false;
     }
 
@@ -64,6 +69,7 @@ public class Wall extends ActionBarActivity implements View.OnTouchListener, Wal
     protected void onStart() {
         super.onStart();
         createBall();
+        //Resets the scoreboard
         score = 0;
     }
 
@@ -88,9 +94,11 @@ public class Wall extends ActionBarActivity implements View.OnTouchListener, Wal
     @Override
     public boolean onTouch(View v, MotionEvent event) {
         switch(event.getAction()){
+            //Sets the paddle to where you press
             case MotionEvent.ACTION_DOWN:
                 x = event.getX();
                 break;
+            //Sets the paddle to where you moved the finger
             case MotionEvent.ACTION_MOVE:
                 x = event.getX();
                 break;
@@ -100,7 +108,12 @@ public class Wall extends ActionBarActivity implements View.OnTouchListener, Wal
         return true;
     }
 
+    /**
+     * Returns the current speed of the ball
+     * @return float representing the ball speed
+     */
     private float getBallSpeed() {
+        //Makes the ball speed up by your score
         if(score == 0){
             return ballSpeed * 15;
         }else{
@@ -108,43 +121,69 @@ public class Wall extends ActionBarActivity implements View.OnTouchListener, Wal
         }
     }
 
+    /**
+     * Initiates the lose sequenze
+     */
     private void ballLost(){
+        //Only opens the lose dialog if another dialog is not already open.
         if(!overlay) {
+            //Gets the highscore from the sharedPreferences
             int highscore = sharedPreferences.getInt(HIGHSCORE, 0);
+            //if the new score is bigger is sets the new score as the high score and saves it.
             if (highscore < score) {
                 saveHighScore();
                 highscore = score;
             }
 
+            //Creates a fragment and send the score and highscore to that fragment
             DialogFragment newFragment = WallLoseDialogFragment.newInstance(score, highscore);
+            //Displays the fragment
             newFragment.show(getFragmentManager(), "lost");
+            //Disables pressing besides the dialog will close
             newFragment.setCancelable(false);
             surfaceView.pause();
+            //Sets overlay to make sure only one dialog will be shown at a time.
             overlay = true;
         }
     }
 
+    /**
+     * Saves the highscore to sharedPreferences
+     */
     private void saveHighScore(){
+        //Saves the score to the sharePreferences
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putInt(HIGHSCORE, score);
 
         editor.commit();
     }
 
+    /**
+     * This code is run when the restart button is pressed
+     * @param dialog
+     */
     @Override
     public void onDialogPositiveClick(DialogFragment dialog){
         surfaceView.resume();
         overlay = false;
         createBall();
+        //Resets the score
         score = 0;
     }
 
+    /**
+     * This code is run when the menu button is pressed
+     * @param dialog
+     */
     @Override
     public void onDialogNegativeClick(DialogFragment dialog){
+        //Finishes the activity
         finish();
     }
 
-
+    /**
+     * This class holds the SurfaceView, which is where all the drawing to the screen is done.
+     */
     public class wallSurface extends SurfaceView implements Runnable{
 
         SurfaceHolder holder;
@@ -158,22 +197,28 @@ public class Wall extends ActionBarActivity implements View.OnTouchListener, Wal
             holder = getHolder();
         }
 
+        /**
+         * Terminates the thread that runs the surface view
+         */
         public void stop() {
-            //while(true){
             try{
                 thread.join();
             }catch(InterruptedException e){
                 e.printStackTrace();
             }
-            //break;
-            //}
             thread = null;
         }
 
+        /**
+         * Pauses the surface view making any animation stop
+         */
         public void pause() {
             isRunning = false;
         }
 
+        /**
+         * Resumes the surface view starting the animations again
+         */
         public void resume(){
             isRunning = true;
             thread = new Thread(this);
@@ -186,31 +231,44 @@ public class Wall extends ActionBarActivity implements View.OnTouchListener, Wal
 
         @Override
         public void run() {
+            //Makes the animation pauseble
             while(isRunning){
                 if(!holder.getSurface().isValid())
                     continue;
 
+                //Locks the canvas so that we can draw on it.
                 Canvas canvas = holder.lockCanvas();
+                //Sets the background color to black
                 canvas.drawRGB(0, 0, 0);
+                //Draws the score to the screen
                 canvas.drawText(Integer.toString(score), canvas.getWidth() / 2, canvas.getHeight() / 4 + metrics.widthPixels / 8, whitePaint);
 
+                //if there haven't been a finger on the screen the paddle is drawn in the middle
                 if(x==0){
                     canvas.drawRect(canvas.getWidth() / 2 - canvas.getWidth() / 8, canvas.getHeight() - (canvas.getHeight() / 25) * 2, canvas.getWidth() / 2 + canvas.getWidth() / 8, canvas.getHeight() - canvas.getHeight() / 25, greenPaint);
                 }
 
+                //Draws the paddle at the position of the finger
                 if(x != 0){
                     canvas.drawRect(x - canvas.getWidth() / 8, canvas.getHeight() - (canvas.getHeight() / 25) * 2, x + canvas.getWidth() / 8, canvas.getHeight() - canvas.getHeight() / 25, greenPaint);
                 }
 
                 moveBall(canvas);
 
+                //Draws the ball
                 canvas.drawCircle(bx, by, br, greenPaint);
 
+                //Unlocks the canvas making it visible on screen
                 holder.unlockCanvasAndPost(canvas);
             }
         }
 
+        /**
+         * Moves the ball to the appropriate place
+         * @param canvas The canvas on where the ball is drawn.
+         */
         private void moveBall(Canvas canvas) {
+            //if the ball has moved out of the bottom it is lost
             if(by - br > canvas.getHeight()){
                 ballLost();
             }
@@ -226,12 +284,16 @@ public class Wall extends ActionBarActivity implements View.OnTouchListener, Wal
             if(by + br > canvas.getHeight() - (canvas.getHeight() / 25) * 2 && bx > x - canvas.getWidth() / 8 && bx < x + canvas.getWidth() / 8){
                 if(by - br < canvas.getHeight() - (canvas.getHeight() / 25) * 2){
                     if(ballMoveDown){
+                        //makes the ball move up
                         ballMoveDown = false;
+                        //Adds one to the score
                         score++;
                     }
                 }
+            //Determent if the ball has hit the top
             }else if(by-br <= 0) ballMoveDown=true;
 
+            //Moves the ball
             if(ballMoveDown){
                 by += getBallSpeed();
             }else{
@@ -243,8 +305,6 @@ public class Wall extends ActionBarActivity implements View.OnTouchListener, Wal
                 bx -= getBallSpeed();
             }
         }
-
-
     }
 
 
--- app/src/main/java/dk/simwir/pingpong/dialogs/WallLoseDialogFragment.java
+++ app/src/main/java/dk/simwir/pingpong/dialogs/WallLoseDialogFragment.java
@@ -16,29 +16,43 @@ public class WallLoseDialogFragment extends DialogFragment{
     private static final String SCORE = "score";
     private static final String HIGHSCORE = "highscore";
 
+    /**
+     * Creates a new instance of the WallLoseDialog, Passing the information needed to show the dialog.
+     * @param score The players score on the current attempt
+     * @param highscore The players highscore on all attempts
+     * @return Returns the fragment needed for creating a new dialog
+     */
     public static WallLoseDialogFragment newInstance(int score, int highscore){
         WallLoseDialogFragment f = new WallLoseDialogFragment();
 
+        //Saves the arguments passed in, in a fragment so that it can be referenced when showing the dialog.
         Bundle args = new Bundle();
         args.putInt(SCORE, score);
         args.putInt(HIGHSCORE, highscore);
         f.setArguments(args);
 
         return f;
-}
+    }
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState){
         String message;
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
+        //Gets the score and the Highscore from the fragment saved later
         mScore = getArguments().getInt(SCORE);
         mHighscore = getArguments().getInt(HIGHSCORE);
+
+        //True if the score from this game is a new highscore
         if(mHighscore==mScore){
+            //Sets the message to be shown in the dialog
             message = getString(R.string.dialog_wall_new_highscore) + "\n" + getString(R.string.dialog_wall_your_highscore) + " " + mHighscore;
         }else{
+            //Sets the message to be shown in the dialog
             message = getString(R.string.dialog_wall_game_over) + " " + mScore + "\n" + getString(R.string.dialog_wall_your_highscore)+ " " + mHighscore;
         }
+        //Sets the message to the message specefied above and sets the positive button to "Restart"
+        //and negative button to "Menu"
         builder.setMessage(message).setPositiveButton(R.string.restart, new DialogInterface.OnClickListener(){
             @Override
             public void onClick(DialogInterface dialog, int which){
@@ -55,6 +69,9 @@ public class WallLoseDialogFragment extends DialogFragment{
 
     LoseDialogListener mListener;
 
+    /**
+     * An interface that makes the activity handle the button press
+     */
     public interface LoseDialogListener{
         public void onDialogPositiveClick(DialogFragment dialog);
 
